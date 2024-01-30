6c9b66d706e2f631db87d7a7f011ada60be16d87
azizaminova <aziza.aminova@testpro.io>
2024-01-30 16:31:15 -0500

    Homework16

---
+++ src/test/java/ProfileTest.java
@@ -0,0 +1,69 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.UUID;
+@Test
+public class ProfileTest extends BaseTest{
+
+    public String changeProfileName() throws InterruptedException {
+        //Task: need to change the username. Need to change the name of profile which we did on the page settings
+
+        //NavigateToPage
+        navigateToPage();
+        //Login With  Correct Credentials
+        provideEmail("demo@class.com");
+        providePassword("te$t$tudent");
+        logintoKoel();
+        Thread.sleep(2000);
+        //Click on Avatar
+        clickAvatarIcon();
+        //Generate random username
+        String randomName=generateRandomName();
+        System.out.println("Random name is: "+ randomName);
+        //Provide current password
+        provideCurrentPassword("te$t$tudent");
+        //Set the new profile name
+        provideProfileName(randomName);
+        Thread.sleep(2000);
+        //Click on save
+        clickSave();
+        Thread.sleep(5000);
+        //Assertion (Expected vs. Actual results)
+        WebElement actualProfileName = driver.findElement(By.cssSelector("a.view-profile.span"));
+        System.out.println(actualProfileName.getText());
+        Assert.assertEquals(actualProfileName.getText(),randomName());
+    }
+
+
+        public void provideProfileName(String newName) {
+            WebElement profileNameField = driver.findElement(By.cssSelector("[name='name']"));
+            profileNameField.clear();
+            profileNameField.sendKeys(newName);
+        }
+
+
+        public String RandomName() {
+            return UUID.randomUUID().toString().replace("-", "");
+        }
+
+
+        public void clickAvatarIcon() {
+            WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+            avatarIcon.click();
+        }
+
+        public void provideCurrentPassword (String current_password){
+            WebElement currentPasswordField = driver.findElement(By.cssSelector("[name='current_password']"));
+            currentPasswordField.clear();
+            currentPasswordField.sendKeys();
+        }
+    }
+
+    public void clickSave() {
+        WebElement saveButton = driver.findElement(By.cssSelector("button.btn-submit"));
+        saveButton.click();
+    }
+
+
