714343ac0996c174e0aad58bad98b7047fa4fef2
azizaminova <aziza.aminova@testpro.io>
2024-02-01 19:39:44 -0500

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,71 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
+
+import java.time.Duration;
 
 public class BaseTest {
 
-    @BeforeSuite
-    static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+    public WebDriver driver;
+    //public String url = "https://qa.koel.app/";
+
+    //Data Providers Start
+    @DataProvider(name="invalidLoginData")
+    public Object[][] getDataFromDataProviders() {
+            return new Object[][]{
+        {"invalid@email.com", "invalidPassword"},
+        {"demo@class.com", ""},
+        {"", ""}
+        {"invalid@email.com", "te$t$tudent"}
+      };
     }
+
+    @BeforeSuite
+    static void setupClass() { WebDriverManager.chromedriver().setup();}
+
+    @BeforeMethod
+    @Parameters ({"BaseURL"})
+    public void launchBrowser(String baseURL) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        navigateToPage(baseURL);
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    //Helper Methods
+
+    public void logintoKoel() {
+        WebElement loginBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+        loginBtn.click();
+        //Thread.sleep(5000);
+    }
+
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password]"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input=[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
+
+    public void navigateToPage(String url) {
+        driver.get(url);
+    }
+
 }
