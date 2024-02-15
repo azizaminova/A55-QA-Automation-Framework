cf95683cb40bf1cceabd3e85c2bca70a085642cb
azizaminova <aziza.aminova@testpro.io>
2024-02-15 16:33:03 -0500

    Create AllSongsPage.java

---
+++ src/test/java/Pages/AllSongsPage.java
@@ -0,0 +1,28 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class AllSongsPage extends BasePage {
+    public AllSongsPage(WebDriver givenDriver) {
+        super(givenDriver);
+    }
+
+    By firstSongsList = By.cssSelector("all-songs tr.song-item:nth-child(1)");
+    By playOption = By.cssSelector(li.playback);
+
+    public void contextClickFirstSong() {
+        WebElement firstSongElement = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.cssSelector(".all-songs tr.song-item:nth-child(1)")));
+        findElement(firstSongsList);
+        actions.contextClick(firstSongElement).perform();
+    }
+
+    public void choosePlay() {
+        //wait.until(ExpectedConditions
+                //.visibilityOfElementLocated(By.cssSelector("li.playback"))).click();
+        findElement(playOption).click();
+    }
+}
