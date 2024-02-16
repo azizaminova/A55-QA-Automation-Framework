65de99821e4d87202ffee29a012e7f584715eed6
azizaminova <aziza.aminova@testpro.io>
2024-02-16 16:14:15 -0500

    Update LoginPage.java

--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.WebDriver;
 
 public class LoginPage extends BasePage {
     public LoginPage(WebDriver givenDriver) {
-        super (givenDriver);
+        super(givenDriver);
 
     }
 
